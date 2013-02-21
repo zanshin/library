0592f1687e2b28f258821693ffd9c3fe5af2e5cb
Mark Nichols <mark@zanshin.net>
2013-02-20 22:52:17 -0600

    Initial commit of Library project.
    Basic Sinatra app to capture music owned and music related books owned.
    
    Add functionality works (mostly)
    Formatting of table to show books owned needs work
    Edit page created
    Delete page created
    
    	new file:   Gemfile
    	new file:   Gemfile.lock
    	new file:   library.db
    	new file:   library.rb
    	new file:   public/bootstrap-responsive.css
    	new file:   public/bootstrap.min.css
    	new file:   public/bootstrap.min.js
    	new file:   public/bootstrap/css/bootstrap-responsive.css
    	new file:   public/bootstrap/css/bootstrap-responsive.min.css
    	new file:   public/bootstrap/css/bootstrap.css
    	new file:   public/bootstrap/css/bootstrap.min.css
    	new file:   public/bootstrap/img/glyphicons-halflings-white.png
    	new file:   public/bootstrap/img/glyphicons-halflings.png
    	new file:   public/bootstrap/js/bootstrap.js
    	new file:   public/bootstrap/js/bootstrap.min.js
    	new file:   public/reset.css
    	new file:   public/style.css
    	new file:   views/delete.erb
    	new file:   views/edit.erb
    	new file:   views/home.erb
    	new file:   views/layout.erb

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+gem "sinatra"
+gem "data_mapper"
+gem "thin"
+gem "dm-sqlite-adapter"
+#gem "pony"
+#gem "redcarpet"
+#gem "fastimage"
---
+++ library.rb
@@ -0,0 +1,82 @@
+require "rubygems"
+require "sinatra"
+require "data_mapper"
+
+# database setup
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/library.db")
+class Book
+    include DataMapper::Resource
+    property :id, Serial
+    property :type, String, :length => 25, :required => true
+    property :composer, String, :length =>75, :required => true
+    property :composition, Text, :required => true
+    property :edition, String, :length => 40, :required => true
+    property :editor, String, :length => 40
+    property :purchase_price, Decimal, :precision => 10, :scale => 2
+    property :created_at, Date
+    property :updated_at, Date
+
+    def to_s
+      "Book{id: #{id}, type: #{type}, composer: #{composer}, composition: #{composition}, edition: #{edition}, editor: #{editor}, price: #{purchase_price}}"
+    end
+end
+DataMapper.finalize.auto_upgrade!
+
+# index action
+get '/' do 
+    @books = Book.all :order => :id.desc
+    puts "found #{@books.count} books"
+    @title = 'All Books'
+    erb :home
+end
+
+#post action (add)
+post '/' do 
+    b = Book.new
+    b.type = params[:type]
+    b.composer = params[:composer]
+    b.composition = params[:composition]
+    b.edition = params[:edition]
+    b.editor = params[:editor]
+    b.purchase_price = params[:price]
+    b.created_at = Time.now.to_date.to_s
+    b.updated_at = Time.now.to_date.to_s
+    
+    if b.save
+        puts "the book says it saved"
+    else
+        puts "something went wrong and it didn't really save: #{b.to_s}"
+    end
+
+    redirect '/'
+end
+
+# edit action
+get '/:id' do 
+    @book = Book.get params[:id]
+    @title = "Edit book"
+    erb :edit
+end
+
+# # put (update) action
+# put '/:id' do 
+#     n = Note.get params[:id]
+#     n.content = params[:content]
+#     n.compelte = params[:complete] ? 1: 0
+#     n.updated_at = Time.now
+#     n.save
+#     redirect '/'
+# end
+
+# delete action
+get '/:id/delete' do 
+    @book = Book.get params[:id]
+    @title = "Confirm deletion of book ##{params[:id]}"
+    erb :delete
+end
+
+delete '/:id' do 
+    b = Book.get params[:id]
+    b.destroy
+    redirect '/'
+end
